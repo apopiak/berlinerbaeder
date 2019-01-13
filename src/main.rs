43c48b2421b47eb4e76f6afcd64939ffa8cb3fd5
Alexander Popiak <alexander.popiak@gmail.com>
2019-01-13 18:02:39 +0100

    allow listing opening times of chosen poll on chosen day

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ use reqwest::Client;
 use reqwest::Url;
 
 use select::document::Document;
-use select::node::{Children, Node};
+use select::node::{Node};
 use select::predicate::*;
-use select::selection::Selection;
 
 error_chain! {
    foreign_links {
@@ -19,64 +18,97 @@ error_chain! {
    }
 }
 
+struct ContainsText<'a>(&'a str);
+
+impl<'a> Predicate for ContainsText<'a> {
+    fn matches(&self, node: &Node) -> bool {
+        node.text().contains(self.0)
+    }
+}
+
 fn run() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 || args.len() > 3 {
+        println!("usage: baeder <Tag> <Badname>");
+    }
+
     let base_address = Url::parse("https://www.berlinerbaeder.de")?;
     let search_address = base_address.join("/baeder/bad-suche/")?;
+
+    let search_day = args.get(1).expect("expecting at least 2 arguments");
+    let empty: String = "".to_owned();
+    let filter_term = args.get(2).unwrap_or(&empty);
+    let filter = !filter_term.is_empty();
+
     let client = Client::new();
     let res = client.get(search_address).send()?;
-
-    let baeder_urls: Vec<Url> = Document::from_read(res)?
-        .find(Name("a"))
-        .filter_map(|n| n.attr("href"))
-        .filter(|l| l.matches("/baeder/").count() > 0)
-        .skip(1)
-        .filter_map(|l| base_address.join(l).ok())
-        // .map(|x| { println!("{:?}", x); x })
+    let baeder_urls: Vec<(String, Url)> = Document::from_read(res)?
+        .find(Name("a").and(Attr("href", ())))
+        .filter(|n| {
+            n.attr("href")
+                .expect("we filtered on href already")
+                .matches("/baeder/")
+                .any(|_| true)
+        })
+        .filter(|n| {
+            if filter {
+                n.find(Attr("itemprop", "name").and(ContainsText(filter_term)))
+                    .any(|_| true)
+            } else {
+                n.find(Attr("itemprop", "name")).any(|_| true)
+            }
+        })
+        .filter_map(|n| {
+            let link = n.attr("href").expect("we filtered on href already");
+            let name = n
+                .find(Attr("itemprop", "name"))
+                .map(|n| n.text())
+                .fold("".to_owned(), |_, t| t);
+            base_address.join(link).ok().map(|url| (name, url))
+        })
         .collect();
 
-    let current_day = "Sonntag";
-    baeder_urls.into_iter().take(1).for_each(|url| {
+    println!("Offen am {}:", search_day);
+    baeder_urls.into_iter().for_each(|(name, url)| {
         client
             .get(url)
             .send()
-            .map_err(move |e| {
+            .map_err(|e| {
                 println!("{:?}", e);
                 e
             })
-            .map(move |res| {
+            .map(|res| {
                 Document::from_read(res)
-                    .map_err(move |e| {
+                    .map_err(|e| {
                         println!("{:?}", e);
                         e
                     })
-                    .map(move |d| {
-                        d.find(Class("day").and(|n: &Node| n.text().contains(current_day)))
-                            .for_each(|n| {
-                                n.find(Class("opentime")).for_each(|time_node| {
-                                    time_node.children().for_each(|time| {
-                                        let open_text = time
-                                            .children()
-                                            .filter(|child| {
-                                                child.is(|c: &Node| {
-                                                    c.attr("data-lng")
-                                                        .unwrap_or("")
-                                                        .matches("en")
-                                                        .count()
-                                                        == 0
-                                                })
+                    .map(|d| {
+                        println!("{}", name);
+                        d.find(Class("tab").and(ContainsText("Badebereich")))
+                            .for_each(|swimming| {
+                                swimming
+                                    .find(Class("day").and(ContainsText(search_day)))
+                                    .for_each(|n| {
+                                        n.find(Class("opentime")).for_each(|time_node| {
+                                            time_node.children().for_each(|time| {
+                                                let open_text = time
+                                                    .children()
+                                                    .filter(|c| c.is(Not(Attr("data-lng", "en"))))
+                                                    .map(|c| c.text().trim().to_owned())
+                                                    .filter(|c| !c.is_empty())
+                                                    .collect::<Vec<String>>()
+                                                    .join(" -> ")
+                                                    .trim()
+                                                    .to_owned();
+                                                if !open_text.is_empty() {
+                                                    println!("{}", open_text);
+                                                }
                                             })
-                                            .map(|c| c.text().trim().to_owned())
-                                            .filter(|c| !c.is_empty())
-                                            .collect::<Vec<String>>()
-                                            .join(" -> ")
-                                            .trim()
-                                            .to_owned();
-                                        if !open_text.is_empty() {
-                                            println!("{}", open_text);
-                                        }
+                                        })
                                     })
-                                })
                             });
+                        println!("");
                     })
                     .ok();
             })
